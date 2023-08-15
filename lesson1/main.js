faea5c390194d47a65264777f6e13de5a0f1db7e
GrayHead <ss.zhuravlov@gmail.com>
2023-08-15 21:36:18 +0300

    lesson 1 variables base etc

---
+++ lesson1/main.js
@@ -0,0 +1,104 @@
+// let a1; // declare
+// a1 = 'hello okten';
+// let a1 = 'hello okten'; // string
+// console.log(a1);
+//
+// let b1 = 123;
+// console.log(b1);
+// b1 = 12313213123;
+//
+// const c1 = 'asdgajgd';
+// // c1 = 111;
+//
+// var v1 = 'hello';
+// console.log(v1);
+// {
+//     var a = 'asd';
+// }
+// console.log(a);
+
+
+// string
+let s1 = 'hello';
+let s2 = "okten";
+let s3 = `!!!!`;
+
+// let result = s1 + ' ' + s2 + ' ' + s3;
+
+let result = `${s1} ${s2} ${s3}`;
+console.log(result);
+
+
+//number
+let n1 = 12;
+let n2 = 5;
+
+let nRes = n1 % n2; // + - * / %
+console.log(nRes);
+
+// boolean
+// > < >= <= == != === !==
+let b1 = 5;
+let b2 = 10;
+let bRes = b1 < b2;
+console.log(bRes);
+
+// console.log(typeof bRes);
+
+
+// let s = 'hello';
+// let n = 100500;
+// let b = true;
+// console.log(s + n);
+// console.log(s + n + b);
+
+
+// number + string = string
+// console.log(10 + 20 + 'xxx');
+// console.log((20 + 20) + 'xxx' + 20)
+// console.log(+'123');
+// console.log(+'xxx')
+// console.log(parseInt('123xxx'));
+// console.log(123.12)
+
+
+//cast
+// console.log(!!"");
+// console.log(!!0);
+// console.log(!!undefined);
+// console.log(!!NaN);
+// console.log(!!false);
+// console.log(!!null);
+//
+//
+// console.log(!!-117828);
+// console.log(!! '0');
+//
+// console.log(!true);
+// console.log(!false);
+// console.log(!!!'asdasd');
+
+let arr = [111, 'okten', true];
+
+//                    0       1      2
+console.log(arr);
+console.log(arr[1]);
+console.log(arr[0]);
+
+let user = {
+    name: 'vasya',
+    age: 13,
+    status: true
+};
+
+console.log(user);
+console.log(user.name)
+console.log(user.age)
+console.log(user.status);
+
+
+
+
+
+
+
