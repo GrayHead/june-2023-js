23e1ece16ccaa8a40a70c9042ce7e6d787b0b47b
GrayHead <ss.zhuravlov@gmail.com>
2023-09-08 19:59:45 +0300

    ts 1

---
+++ typescript/lesson1/main.js
@@ -0,0 +1,31 @@
+var str = 'asdas';
+str = 'qwewqe';
+// str = 1000
+// str = true;
+var num;
+//....
+//....
+//....
+//....
+//....
+//....
+num = 100500;
+// num = 'asgdjag';
+// num = true;
+var bool = true;
+// bool = 100500;
+// const x: number = '123';
+var numbers = [213, 123];
+var strings = ['ashdgjsahgd', 'qweqw' /*,123*/];
+var obj;
+obj = {
+    id: 123,
+    name: 'ygsgfsd',
+    skills: ['asdads', 'qweqwewq']
+};
+var obj2 = {
+    id: 2,
+    name: 'asdasdsa',
+    skills: ['asd', 'qwe'],
+    status: true
+};
